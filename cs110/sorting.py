1ad36a92c00e3eb5bd90f83e5b1809709dff66a9
Guy Davidson <guydav@gmail.com>
2016-09-09 12:04:25 +0200

    Adding merge sort for CS110 2.1

--- cs110/sorting.py
+++ cs110/sorting.py
@@ -79,19 +79,98 @@ def selection_sort(input_list):
         input_list.insert(i, input_list.pop(min_index))
 
 
+@profile(sort='calls', immediate=True)
+def merge_sort(input_list, start=0, end=None):
+    if end is None:
+        end = len(input_list)
+
+    # print 'merge sort called, start={start}, end={end}'.format(start=start, end=end)
+
+    # Adding two as it's an open-ended interval
+    if start + 2 < end:
+        mid = start + (end - start) / 2
+        merge_sort(input_list, start, mid)
+        merge_sort(input_list, mid, end)
+    else:
+        mid = None
+
+    merge(input_list, start, mid, end)
+
+
+"""
+Merges the list from start to end, assumes the sublists
+[start, mid) and [mid, end) are sorted relative to each other
+
+# if left_index == mid:
+#     new_sub_list.append(input_list[right_index])
+#     right_index += 1
+#
+# elif right_index == end:
+#     new_sub_list.append(input_list[left_index])
+#     left_index += 1
+#
+# elif compare_gt(input_list, left_index, right_index):
+#     new_sub_list.append(input_list[right_index])
+#     right_index += 1
+#
+# else:
+#     new_sub_list.append(input_list[left_index])
+#     left_index += 1
+"""
+def merge(input_list, start, mid, end):
+    # print 'merge called, start={start}, mid={mid}, end={end}'.format(start=start, mid=mid, end=end)
+
+    if start + 1 == end:
+        return
+
+    # the case where start + 1 == end, we might have to swap
+    if mid is None:
+        if compare_gt(input_list, start, end - 1):
+            swap(input_list, start, end - 1)
+
+    # the more complicated case, of merging two sub-lists
+    else:
+        new_sub_list = []
+        left_index = start
+        right_index = mid
+        while (left_index < mid) or (right_index < end):
+            if (left_index == mid) or (right_index != end and compare_gt(input_list, left_index, right_index)):
+                new_sub_list.append(input_list[right_index])
+                right_index += 1
+
+            else:
+                new_sub_list.append(input_list[left_index])
+                left_index += 1
+
+        input_list[start:end] = new_sub_list
+
+    # print input_list
+
 def main():
     # 1.1
     # run_function(lambda x: [0] * x, 2, 20)
 
     # 1.2
-    ints = [i for i in random.random_integers(0, 100, 100)]
-    # print ints
+    # ints = [i for i in random.random_integers(0, 10000, 10000)]
+    # # print ints
+    #
+    # for sort_func in [insertion_sort, shell_sort, bubble_sort, selection_sort]:
+    #     ints_copy = ints[:]
+    #     sort_func(ints_copy)
+    #     print sort_func.func_name
+    #     # print ints_copy
 
-    for sort_func in [insertion_sort, shell_sort, bubble_sort, selection_sort]:
+    # 2.1
+    ints = [i for i in random.random_integers(0, 1000, 1000)]
+    # print ints
+    # merge_sort(ints)
+    # print ints
+    # print all(ints[i] <= ints[i + 1] for i in xrange(len(ints) - 1))
+
+    for sort_func in [insertion_sort, selection_sort, merge_sort]:
         ints_copy = ints[:]
         sort_func(ints_copy)
-        print sort_func.func_name
-        # print ints_copy
+        print sort_func.func_name, all(ints_copy[i] <= ints_copy[i + 1] for i in xrange(len(ints_copy) - 1))
 
 
 if __name__ == '__main__':
