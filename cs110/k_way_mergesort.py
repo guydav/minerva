1e2899b1bc9b0980cc79866b54693489d9e3e762
Guy Davidson <guydav@gmail.com>
2016-09-17 00:13:23 +0200

    Final versions of the merge-sort stuff

--- cs110/k_way_mergesort.py
+++ cs110/k_way_mergesort.py
@@ -1,9 +1,14 @@
 from profilehooks import profile
-from numpy import random
+from numpy import random, average
+import pstats
+import os
+import tabulate
+import matplotlib.pyplot as plt
 
-
+# A global flag to set of debug statements should be printed to stdout or not
 DEBUG = False
-
+# A path for profiling outputs
+PROFILE_STATS_PATH = '/tmp/profile.stats'
 
 def compare_gt(input_list, a, b):
     """
@@ -38,7 +43,37 @@ def is_sorted(input_list):
     """
     return all(input_list[i] <= input_list[i + 1] for i in xrange(len(input_list) - 1))
 
-@profile(sort='calls', immediate=True)
+
+@profile(sort='calls', immediate=True, stdout=False, filename=PROFILE_STATS_PATH)
+def profiled_insertion_sort(input_list, start=0, end=None):
+    """
+    Insertion sort, implemnted from pseudo code by Cormen et al. (CLRS). Assumes the list isn't already sorted.
+
+    A copy of the other insertion sort method, to use when profiling, in order to allow for cleaner printing
+    of profiling outcomes (one profiled function calling another does not print neatly)
+
+    :param input_list: the list to sort
+    :param start: the index to start from, defaults to 0
+    :param end: the index to end on, defaults to the length of the list, exclusive (Python list convention)
+    :return: None, with the list sorted in-place
+    """
+    list_length = len(input_list)
+
+    if end is None:
+        end = list_length
+
+    # start+1 as a list of length 1 is always sorted
+    for i in xrange(start+1, end):
+        current_value = input_list[i]
+        j = i - 1
+
+        while (j >= start) and (input_list[j] > current_value):
+            input_list[j + 1] = input_list[j]
+            j -= 1
+
+        input_list[j + 1] = current_value
+
+
 def insertion_sort(input_list, start=0, end=None):
     """
     Insertion sort, implemnted from pseudo code by Cormen et al. (CLRS). Assumes the list isn't already sorted.
@@ -64,7 +99,7 @@ def insertion_sort(input_list, start=0, end=None):
         input_list[j + 1] = current_value
 
 
-@profile(sort='calls', immediate=True)
+@profile(sort='calls', immediate=True, stdout=False, filename=PROFILE_STATS_PATH)
 def k_way_merge_sort(input_list, k=3, start=0, end=None, insertion_sort_threshold=None):
     """
     Fully flexible k-way merge sort. Default value of k=3 makes it a three-way
@@ -77,6 +112,8 @@ def k_way_merge_sort(input_list, k=3, start=0, end=None, insertion_sort_threshol
             defaults to 0, to allow ignoring the parameter when calling on the whole array
     :param end: the end index of the subarray currently being processed, exclusive (Python list convention)
             defaults to the length of the list to allow ignoring when calling on the whole array
+    :param insertion_sort_threshold: The threshold under which to call insertion sort rather than merge.
+            defaults to None, which will mean never.
     :return: None, with the list sorted in-place
     """
     list_length = len(input_list)
@@ -89,7 +126,7 @@ def k_way_merge_sort(input_list, k=3, start=0, end=None, insertion_sort_threshol
 
     # if k <= 1, we treat it as k = 1, which is insertion sort, so we just call that instead
     # Additionally, checks the insertion sort threshold, where below a certain list length we just use insertion sort
-    if (k <= 1) or (insertion_sort_threshold and end - start <= insertion_sort_threshold):
+    if (k <= 1) or (insertion_sort_threshold is not None and end - start <= insertion_sort_threshold):
         return insertion_sort(input_list, start, end)
 
     # If the current subarray is smaller than k, just merge it directly, otherwise split it
@@ -194,20 +231,53 @@ def merge(input_list, start, end, subarray_indices):
         print input_list
 
 
-def main():
+def k_way_function_params(input_list, k_function, threshold_function):
+    length = len(input_list)
+    k_way_merge_sort(input_list, k=k_function(length), insertion_sort_threshold=threshold_function(length))
+
+
+def optimization_function(params, training_set):
+    """
+    A function designed to attempt to optimize parameters for the insertion-sort threshold variant of
+    k-way merge sort. Ended up using it to brute force through parameters. It calculates the time taken
+    for every collection to sort in the training set, and returns the average time
+    :param params: The parameters to pass to k_way_merge_sort - assumed to be (k, insertion_sort_threshold)
+    :param training_set: An iterable of collections to sort (by default a list of lists)
+    :return: The average time taken to sort every collection in the training set
+    """
+    results = []
+    k, insertion_sort_threshold = params
+
+    for test_list in training_set:
+        k_way_merge_sort(test_list, k=int(k), insertion_sort_threshold=int(insertion_sort_threshold))
+        results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+    avg = average(results)
+
+    if DEBUG:
+        print k, insertion_sort_threshold, avg
+    return avg
+
+
+def various_testing_routines():
+    """
+    A number of various testing routines developed while checking implementations
+    :return: None
+    """
     # Stress / random testing code:
-    # for _ in xrange(100):
+    # for i in xrange(100):
     #     if 0 == (i % 10):
     #         print i
     #
     #     for k in xrange(2, 21):
     #         ints = [i for i in random.random_integers(0, 1000, 1000)]
     #         ints_copy = ints[:]
-    #         k_way_merge_sort(ints, k=k)
+    #         k_way_merge_sort(ints_copy, k=k)
     #
-    #         if not is_sorted(ints):
-    #             print ints_copy
+    #         if not is_sorted(ints_copy):
     #             print ints
+    #             print ints_copy
     #             print k
 
     # More exhaustion testing, this time of all possible values of k
@@ -231,8 +301,120 @@ def main():
     #         print threshold
     #         print ints_copy
     #         print ints
-    ints = [i for i in random.random_integers(0, 10000, 10000)]
-    k_way_merge_sort(ints, k=16, insertion_sort_threshold=16)
+
+    # ints = [i for i in random.random_integers(0, 10000, 10000)]
+    # profiled_insertion_sort(ints)
+    # k_way_merge_sort(ints[:], k=10)
+    # k_way_merge_sort(ints[:], k=10, insertion_sort_threshold=10)
+
+    return
+
+
+def brute_force_parameter_search():
+    """
+    Perform brute force searches in attempt to find optimal parameters for the insertion-sort optimized merge sort.
+    First search performed was a wide search over 2-100 in jumps of five, and the second one was a narrower one,
+    in the range [10, 30, 2]x[30, 60, 3] for k and the insertion sort threshold.
+
+    I attempted to perform more complex optimization routines, but they are problematic for at least three reasons:
+    1) There's no guarantee the average running time is convex - there could clearly be local optima.
+    2) Measuring time is not necesarily a good proxy, as it might depends on other OS processes and scheduling issues
+    (e.g., it is unclear to me if time measured is while this process is running, or globally). Number of function calls
+    is also a flawed measure, as the recursive implementation of merge sort makes it take many more calls than insertion
+     sort. The number of CPU cycles required would likely give a better measure.
+    3) The parameters are integer-valued, which makes optimizing them far more annoying than a scalar-valued function.
+     I looked into linear/integer programming for a few minutes, and decided to eschew it for now due to reasons 1 and 2.
+    :return: None
+    """
+
+    training_set = [[i for i in random.random_integers(0, 10000, random.randint(100, 10000))] for _ in xrange(20)]
+
+    min_k = 0
+    min_threshold = 0
+    min_average = 1
+
+    # first search - wide search over 2-100 in jumps of five - results were 22,57, 22,32
+    for k in xrange(2, 101, 5):
+        for threshold in xrange(2, 101, 5):
+            avg = optimization_function((k, threshold), training_set)
+            if avg < min_average:
+                min_average = avg
+                min_k = k
+                min_threshold = threshold
+
+    print 'First search, [2, 101, 5] x [2, 101, 5]', min_k, min_threshold, min_average
+    min_k = 0
+    min_threshold = 0
+    min_average = 1
+
+    # second search - narrow search over k in [10, 30, 2], threshold in [30, 60, 3]  -
+    for k in xrange(10, 32, 2):
+        for threshold in xrange(30, 63, 3):
+            avg = optimization_function((k, threshold), training_set)
+            if avg < min_average:
+                min_average = avg
+                min_k = k
+                min_threshold = threshold
+
+    print 'Second search, [10, 30, 2] x [30, 60, 3]', min_k, min_threshold, min_average
+
+
+FINAL_K = 22
+FINAL_THRESHOLD = 33
+LIST_SIZES = (10, 30, 60, 100, 300, 600, 1000, 3000, 6000, 10000, 30000, )
+
+
+def performance_graphs():
+    insertion_sort_results = []
+    three_way_results = []
+    k_way_with_threshold_results = []
+    k_way_func_params_results = []
+
+    for size in LIST_SIZES:
+        print 'Current size', size
+        current_list = [i for i in random.random_integers(0, 10000, size)]
+        profiled_insertion_sort(current_list[:])
+        insertion_sort_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+        k_way_merge_sort(current_list[:])
+        three_way_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+        k_way_merge_sort(current_list[:], k=FINAL_K, insertion_sort_threshold=FINAL_THRESHOLD)
+        k_way_with_threshold_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+        k_way_function_params(current_list[:], lambda l: int(l ** 0.33), lambda l: 33)
+        k_way_func_params_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+    print(tabulate.tabulate([["Algorithm / Size"] + list(LIST_SIZES),
+                             ["Insertion Sort"] + insertion_sort_results,
+                             ["3-way merge sort"] + three_way_results,
+                             ["k-way merge sort with insertion"] + k_way_with_threshold_results,
+                             ["k-way with cubic root k and insertion"] + k_way_func_params_results, ]))
+
+    plt.figure(figsize=(12, 8), dpi=400)
+
+    plt.loglog(LIST_SIZES, insertion_sort_results, basex=10, basey=10, c='r', marker='.', label='Insertion Sort')
+    plt.loglog(LIST_SIZES, three_way_results, basex=10, basey=10, c='b', marker='.', label='Three-way Merge Sort')
+    plt.loglog(LIST_SIZES, k_way_with_threshold_results, basex=10, basey=10, c='b', marker='.',
+               label='K-way Merge-Insertion Sort')
+    plt.loglog(LIST_SIZES, k_way_func_params_results, basex=10, basey=10, c='c', marker='.',
+               label='Cubic root K-way Merge-Insertion Sort')
+
+    plt.grid(True)
+    plt.title("Sorting algorithm performance, double log scale")
+    plt.xlabel("List size, log scale (base 10)")
+    plt.ylabel("Sorting time, log scale (base 10)")
+    plt.legend(loc='lower right')
+
+    plt.show()
+
+
+def main():
+    performance_graphs()
 
 
 if __name__ == '__main__':
