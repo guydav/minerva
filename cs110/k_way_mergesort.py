74883034d53acf787a1da1ce0eda7b7e22da7924
Guy Davidson <guydav@gmail.com>
2016-09-18 10:35:35 +0200

    Final version - more fixes / optimizations

--- cs110/k_way_mergesort.py
+++ cs110/k_way_mergesort.py
@@ -10,6 +10,7 @@ DEBUG = False
 # A path for profiling outputs
 PROFILE_STATS_PATH = '/tmp/profile.stats'
 
+
 def compare_gt(input_list, a, b):
     """
     Comparison of two places in a list extracted to a function.
@@ -63,7 +64,7 @@ def profiled_insertion_sort(input_list, start=0, end=None):
         end = list_length
 
     # start+1 as a list of length 1 is always sorted
-    for i in xrange(start+1, end):
+    for i in xrange(start + 1, end):
         current_value = input_list[i]
         j = i - 1
 
@@ -88,7 +89,7 @@ def insertion_sort(input_list, start=0, end=None):
         end = list_length
 
     # start+1 as a list of length 1 is always sorted
-    for i in xrange(start+1, end):
+    for i in xrange(start + 1, end):
         current_value = input_list[i]
         j = i - 1
 
@@ -126,16 +127,16 @@ def k_way_merge_sort(input_list, k=3, start=0, end=None, insertion_sort_threshol
 
     # if k <= 1, we treat it as k = 1, which is insertion sort, so we just call that instead
     # Additionally, checks the insertion sort threshold, where below a certain list length we just use insertion sort
-    if (k <= 1) or (insertion_sort_threshold is not None and end - start <= insertion_sort_threshold):
+    if (k <= 1) or ((insertion_sort_threshold is not None) and (end - start <= insertion_sort_threshold)):
         return insertion_sort(input_list, start, end)
 
     # If the current subarray is smaller than k, just merge it directly, otherwise split it
     subarray_indices = generate_subarrays(start, end, k)
     if subarray_indices:
         for (subarray_start, subarray_end) in subarray_indices:
-            k_way_merge_sort(input_list, k, subarray_start, subarray_end)
+            k_way_merge_sort(input_list, k, subarray_start, subarray_end, insertion_sort_threshold)
 
-    merge(input_list, start, end, subarray_indices)
+    k_way_merge(input_list, start, end, subarray_indices)
 
 
 def generate_subarrays(start, end, k):
@@ -144,6 +145,10 @@ def generate_subarrays(start, end, k):
     Attempts to generate subarrays of as equal length as possible.
     Once it finds a size that evenly divides the remaining part into the remaining number of subarrays,
     stops dividing each time to find the current size, and uses the stable one.
+
+    This could be optimized even again, by performing the modulo once and knowing that we have
+    ((end - start) % k) "longer" subarrays, whose length is one more than the regular length, which
+    is floor((end - start) / k)
 
     :param start: the start index of the the current subarray to divide
     :param end: the end index of the the current subarray to divide, exclusive (Python list convention)
@@ -182,7 +187,7 @@ def generate_subarrays(start, end, k):
     return subarray_indices
 
 
-def merge(input_list, start, end, subarray_indices):
+def k_way_merge(input_list, start, end, subarray_indices):
     """
     Merge the subarrays in the section between start and end. Deals with several edge cases:
     - If there's one element, do nothing (one element is always sorted)
@@ -208,27 +213,100 @@ def merge(input_list, start, end, subarray_indices):
     if start + 2 == end:
         if compare_gt(input_list, start, end - 1):
             swap(input_list, start, end - 1)
+        return
 
     # If there are no subarray indices, we're merging "lists" of length 1, which is equivalent to insertion sort:
     if subarray_indices is None:
         insertion_sort(input_list, start, end)
+        return
 
     # the more complicated case, of merging actual sub-lists
-    else:
-        sub_lists = [input_list[start_index : end_index] for (start_index, end_index) in subarray_indices]
+    sub_lists = [input_list[start_index: end_index] for (start_index, end_index) in subarray_indices]
 
-        for index in xrange(start, end):
-            min_sub_list_index = 0
-            for current_sub_list_index in xrange(1, len(sub_lists)):
-                if sub_lists[current_sub_list_index][0] < sub_lists[min_sub_list_index][0]:
-                    min_sub_list_index = current_sub_list_index
+    for index in xrange(start, end):
+        min_sub_list_index = 0
+        for current_sub_list_index in xrange(1, len(sub_lists)):
+            if sub_lists[current_sub_list_index][0] < sub_lists[min_sub_list_index][0]:
+                min_sub_list_index = current_sub_list_index
 
-            input_list[index] = sub_lists[min_sub_list_index].pop(0)
-            if not sub_lists[min_sub_list_index]:
-                sub_lists.pop(min_sub_list_index)
+        input_list[index] = sub_lists[min_sub_list_index].pop(0)
+        if not sub_lists[min_sub_list_index]:
+            sub_lists.pop(min_sub_list_index)
 
     if DEBUG:
         print input_list
+
+
+@profile(sort='calls', immediate=True, stdout=False, filename=PROFILE_STATS_PATH)
+def merge_sort(input_list, start=0, end=None, insertion_sort_threshold=None):
+    """
+    My original merge sort implementation, optimized for two sublists at a given time -
+    which is particularly relevant during merging.
+
+    Added an insertion-sort threshold to this implementation as well, to see if it can
+    beat out the non-naive one.
+
+    :param input_list: The list to be sorted in-place, assumed not to be sorted already.
+    :param start: Optional, the starting index. If omitted assumed to be 0.
+    :param end: Optional, the end index. If omitted assumed to be len(input_list).
+                Thus, it is always treated as open-ended, and never included in the sort.
+    :return: None, with the list sorted between start and end
+    """
+    if end is None:
+        end = len(input_list)
+
+    if DEBUG:
+        print 'merge sort called, start={start}, end={end}'.format(start=start, end=end)
+
+    if (insertion_sort_threshold is not None) and (end - start <= insertion_sort_threshold):
+        insertion_sort(input_list, start, end)
+        return
+
+    # Adding two as it's an open-ended interval
+    mid = None
+    if start + 2 < end:
+        mid = start + (end - start) / 2
+        merge_sort(input_list, start, mid)
+        merge_sort(input_list, mid, end)
+
+    merge(input_list, start, mid, end)
+
+
+def merge(input_list, start, mid, end):
+    """
+    The original merge implementation, optimized for merging two-sublists.
+    :param input_list: The list to merge in.
+    :param start: The index to start from
+    :param mid: The index of the middle - if it does not exist, assumed to be a slice of length 1 or 2.
+    :param end: The index to end at, open-ended - not included in merging.
+    """
+    if DEBUG:
+        print 'merge called, start={start}, mid={mid}, end={end}'.format(start=start, mid=mid, end=end)
+
+    # The degenerate case - "merging" a list of length 1
+    if start + 1 == end:
+        return
+
+    # The less-degenerate case - potentially swapping two elements
+    if mid is None:
+        if input_list[start] > input_list[end - 1]:
+            swap(input_list, start, end - 1)
+
+    # The "real" case, of merging two sub-lists, at least one of whom is longer than one.
+    else:
+        new_sub_list = []
+        left_index = start
+        right_index = mid
+        while (left_index < mid) or (right_index < end):
+            if (left_index == mid) or (right_index != end and input_list[left_index] > input_list[right_index]):
+                new_sub_list.append(input_list[right_index])
+                right_index += 1
+
+            else:
+                new_sub_list.append(input_list[left_index])
+                left_index += 1
+
+        input_list[start:end] = new_sub_list
 
 
 def k_way_function_params(input_list, k_function, threshold_function):
@@ -327,54 +405,73 @@ def brute_force_parameter_search():
     :return: None
     """
 
-    training_set = [[i for i in random.random_integers(0, 10000, random.randint(100, 10000))] for _ in xrange(20)]
+    training_set = [list(random.random_integers(0, 10000, random.randint(100, 10000))) for _ in xrange(20)]
+
+    # min_k = 0
+    # min_threshold = 0
+    # min_average = 1
+    # first search - wide search over some intervals - seemed to suggest k { [10, 25], thresh ~ 900
+    # for k in xrange(2, 32, 10):
+    #     for threshold in xrange(2, 2002, 100):
+    #         print k, threshold
+    #         avg = optimization_function((k, threshold), training_set)
+    #         if avg < min_average:
+    #             min_average = avg
+    #             min_k = k
+    #             min_threshold = threshold
+    #
+    # print 'First search, [2, 101, 5] x [2, 101, 5]', min_k, min_threshold, min_average
 
     min_k = 0
     min_threshold = 0
     min_average = 1
 
-    # first search - wide search over 2-100 in jumps of five - results were 22,57, 22,32
-    for k in xrange(2, 101, 5):
-        for threshold in xrange(2, 101, 5):
+    # second search - narrow search over k in [6, 26, 2], threshold in [800, 1000, 20]  -
+    for k in xrange(6, 26, 2):
+        for threshold in xrange(800, 1001, 20):
             avg = optimization_function((k, threshold), training_set)
+            print k, threshold, avg
             if avg < min_average:
                 min_average = avg
                 min_k = k
                 min_threshold = threshold
 
-    print 'First search, [2, 101, 5] x [2, 101, 5]', min_k, min_threshold, min_average
-    min_k = 0
-    min_threshold = 0
-    min_average = 1
-
-    # second search - narrow search over k in [10, 30, 2], threshold in [30, 60, 3]  -
-    for k in xrange(10, 32, 2):
-        for threshold in xrange(30, 63, 3):
-            avg = optimization_function((k, threshold), training_set)
-            if avg < min_average:
-                min_average = avg
-                min_k = k
-                min_threshold = threshold
-
-    print 'Second search, [10, 30, 2] x [30, 60, 3]', min_k, min_threshold, min_average
+    print 'Second search, [6, 26, 2] x [800, 1000, 20]', min_k, min_threshold, min_average
 
 
-FINAL_K = 22
-FINAL_THRESHOLD = 33
-LIST_SIZES = (10, 30, 60, 100, 300, 600, 1000, 3000, 6000, 10000, 30000, )
+# Values selected after running the brute force parameter search several times
+# for randomly generated list sizes between 100 and 10000
+FINAL_K = 22  # 10
+FINAL_THRESHOLD = 33  # 1000
+LIST_SIZES = (10, 30, 60, 100, 300, 600, 1000, 3000, 6000, 10000, 30000, 60000, 100000,)
+INSERTION_SORT_MAX = 10000
 
 
 def performance_graphs():
     insertion_sort_results = []
+    merge_sort_results = []
+    merge_sort_with_threshold_results = []
     three_way_results = []
     k_way_with_threshold_results = []
     k_way_func_params_results = []
 
     for size in LIST_SIZES:
-        print 'Current size', size
+        if DEBUG:
+            print 'Current size', size
+
         current_list = [i for i in random.random_integers(0, 10000, size)]
-        profiled_insertion_sort(current_list[:])
-        insertion_sort_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+
+        if size <= INSERTION_SORT_MAX:
+            profiled_insertion_sort(current_list[:])
+            insertion_sort_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+            os.remove(PROFILE_STATS_PATH)
+
+        merge_sort(current_list[:])
+        merge_sort_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
+        os.remove(PROFILE_STATS_PATH)
+
+        merge_sort(current_list[:], insertion_sort_threshold=100)
+        merge_sort_with_threshold_results.append(pstats.Stats(PROFILE_STATS_PATH).total_tt)
         os.remove(PROFILE_STATS_PATH)
 
         k_way_merge_sort(current_list[:])
@@ -390,16 +487,24 @@ def performance_graphs():
         os.remove(PROFILE_STATS_PATH)
 
     print(tabulate.tabulate([["Algorithm / Size"] + list(LIST_SIZES),
-                             ["Insertion Sort"] + insertion_sort_results,
+                             ["Insertion sort"] + insertion_sort_results + ['N/A'] * 3,
+                             ["Optimized merge sort"] + merge_sort_results,
+                             ["Optimized merge sort with insertion"] + merge_sort_with_threshold_results,
                              ["3-way merge sort"] + three_way_results,
                              ["k-way merge sort with insertion"] + k_way_with_threshold_results,
                              ["k-way with cubic root k and insertion"] + k_way_func_params_results, ]))
 
     plt.figure(figsize=(12, 8), dpi=400)
 
-    plt.loglog(LIST_SIZES, insertion_sort_results, basex=10, basey=10, c='r', marker='.', label='Insertion Sort')
-    plt.loglog(LIST_SIZES, three_way_results, basex=10, basey=10, c='b', marker='.', label='Three-way Merge Sort')
-    plt.loglog(LIST_SIZES, k_way_with_threshold_results, basex=10, basey=10, c='b', marker='.',
+    plt.loglog(LIST_SIZES[:10], insertion_sort_results, basex=10, basey=10, c='r', marker='.',
+               label='Insertion Sort')
+    plt.loglog(LIST_SIZES, merge_sort_results, basex=10, basey=10, c='m', marker='.',
+               label='Optimized Merge Sort')
+    plt.loglog(LIST_SIZES, merge_sort_with_threshold_results, basex=10, basey=10, c='#ffa500', marker='.',
+               label='Optimized Merge Sort With Insertion')
+    plt.loglog(LIST_SIZES, three_way_results, basex=10, basey=10, c='b', marker='.',
+               label='Three-way Merge Sort')
+    plt.loglog(LIST_SIZES, k_way_with_threshold_results, basex=10, basey=10, c='g', marker='.',
                label='K-way Merge-Insertion Sort')
     plt.loglog(LIST_SIZES, k_way_func_params_results, basex=10, basey=10, c='c', marker='.',
                label='Cubic root K-way Merge-Insertion Sort')
