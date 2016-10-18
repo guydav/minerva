a957cacf927436a1b3571655baf5fa9c797261cb
Guy Davidson <guydav@gmail.com>
2016-10-18 19:40:43 +0200

    Implemented pre-class work for 7.2

--- cs110/hash.py
+++ cs110/hash.py
@@ -133,7 +133,7 @@ def search_word_bank(hash_table, hash_function, word_bank):
         contains(hash_table, word, hash_function)
 
 
-def main():
+def main_7_1():
     word_bank = [random_word(WORD_LENGTH) for _ in xrange(WORD_BANK_SIZE)]
     longer_word_bank = [random_word(WORD_LENGTH * 2) for _ in xrange(WORD_BANK_SIZE)]
     hash_funcs = [hash_str1, hash_str2, hash_str3, hash_str4]
@@ -156,5 +156,98 @@ def main():
         search_word_bank(table, hash_func, longer_word_bank)
 
 
+# --------- 7.2 material starts here --------- #
+
+
+"""
+Python dictionaries -
+
+1) On the surface, setting a value for a key that already exists replaces the value.
+In the underlying hash table, it uses a probing defined by the following recurrence:
+    j = ((5*j) + 1) mod 2**i, table size 2 ** i
+and adding a perturbation term to account for multiple collisions over the lower bits:
+    j = (5*j) + 1 + perturb;
+    perturb >>= PERTURB_SHIFT;
+    use j % 2**i as the next table index;
+
+2) Every time the load factor is over 2/3, the table is resized, by setting the new table
+size to the first power of two larger than 4 * num_items if the table is small, or
+2 * num_items if it's large - using 4x early on to keep the table sparse and reduce
+the number of resizing operations performed. The items are then remapped to the table
+using the new table size.
+
+(http://www.laurentluce.com/posts/python-dictionary-implementation/,
+http://svn.python.org/projects/python/trunk/Objects/dictobject.c)
+
+Real dictionaries -
+
+1)
+if not word in dict:
+    print word, 'is mispelled'
+
+2) for a corpus of m words and a dictionary of size n, this is o(m), rather than o(m * log(n))
+
+3) Add commonly misspelled words to the table, with their matching value being the word (or words)
+that the user might have been attempting to spell correctly. Correctly spelled words would have
+a value of true, or an empty list, or some other dummy value.
+"""
+
+
+class CuckooHash(object):
+    def __init__(self, size, *hash_functions):
+        self.table = [None] * size
+        self.hash_used = [None] * size
+        self.hash_functions = hash_functions
+
+    def insert(self, item):
+        """
+        Assuming h hash functions and a table of size m, the complexity of insertion is
+        O(h) + O(h^2) = O(h^2) => nested search in case of collision
+        :param item: the item to be inserted
+        :return: True if insertion succeeded; False if it didn't.
+        """
+        current_keys = []
+        for i in xrange(len(self.hash_functions)):
+            key = self.hash_functions[i](item)
+            current_keys.append(key)
+
+            if self.table[key] is None:
+                self.table[key] = item
+                self.hash_used[key] = i
+                return True
+
+        # if we reached here, none of the functions worked, we can try to shift
+        for k in xrange(current_keys):
+            key = current_keys[k]
+            collision_item = self.table[item]
+
+            for i in xrange(self.hash_used[key] + 1, len(self.hash_functions)):
+                new_key = self.hash_functions[i](collision_item)
+
+                if self.table[new_key] is None:
+                    self.table[new_key] = collision_item
+                    self.hash_used[new_key] = i
+                    self.table[key] = item
+                    self.hash_used[key] = k
+                    return True
+
+        return False
+
+    def search(self, item):
+        """
+        Assuming h hash functions and a table of size m, the complexity of quertying is O(h)
+        :param item: the item to be searched for
+        :return: True if it is in the table; False if it is not.
+        """
+        for i in xrange(len(self.hash_functions)):
+            key = self.hash_functions[i](item)
+
+            if (self.table[key] is not None) and (self.hash_used[key] == i):
+                return True
+
+        return False
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    pass
