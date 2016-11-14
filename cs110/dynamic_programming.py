fe1410ec95517ab7377809172bb73a6df74d6cd2
Guy Davidson <guydav@gmail.com>
2016-11-14 10:02:05 +0100

    Some changes to dynamic programming code for 11.1

--- cs110/dynamic_programming.py
+++ cs110/dynamic_programming.py
@@ -1,6 +1,6 @@
-from functools import partial
-from timeit import timeit
-from numpy.random import randint
+import numpy
+import heapq
+
 
 NEGATIVE_INFINITY = float("-inf")
 DEFAULT_PRICES = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
@@ -89,7 +89,7 @@ def bottom_up_cut_rod(n, prices=DEFAULT_PRICES):
     return cache[n]
 
 
-def extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES):
+def extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES, c=0):
     if n <= 0:
         return [0], [0]
 
@@ -99,8 +99,9 @@ def extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES):
     for j in xrange(1, n + 1):
         max_price = NEGATIVE_INFINITY
 
-        for i in xrange(1, j + 1):
-            new_price = prices[i] + r_cache[j - i]
+        for i in xrange(1, min(j + 1, len(prices))):
+
+            new_price = prices[i] + r_cache[j - i] - c
             if max_price < prices[i] + r_cache[j - i]:
                 max_price = new_price
                 s_cache[j] = i
@@ -110,11 +111,253 @@ def extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES):
     return r_cache, s_cache
 
 
-def print_extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES):
-    r_cache, s_cache = extended_bottom_up_cut_rod(n, prices)
+def print_extended_bottom_up_cut_rod(n, prices=DEFAULT_PRICES, c=0):
+    r_cache, s_cache = extended_bottom_up_cut_rod(n, prices, c)
     while n > 0:
         current_cut = s_cache[n]
         print current_cut, " ",
         n -= current_cut
 
     print
+
+
+def bottom_up_matrix_multiplication_one_based(p):
+    n = len(p) - 1
+    m = numpy.zeros((n, n))
+    s = numpy.zeros((n - 1, n))
+
+    for l in xrange(2, n + 1):
+        for i in xrange(1, n - l + 2):
+            j = i + l - 1
+            m[i - 1][j - 1] = float("inf")
+
+            for k in xrange(i, j):
+                q = m[i - 1][k - 1] + m[k][j - 1] + p[i - 1] * p[k] * p[j]
+                if q < m[i - 1][j - 1]:
+                    m[i - 1][j - 1] = q
+                    s[i - 1][j - 1] = int(k)
+
+    return m, s
+
+
+def print_optimal_matrix_one_based(s, i, j):
+    if i == j:
+        print 'A{i}'.format(i=i),
+
+    else:
+        print '(',
+        print_optimal_matrix_one_based(s, i, int(s[i - 1][j - 1]))
+        print_optimal_matrix_one_based(s, int(s[i - 1][j - 1]) + 1, j)
+        print ')',
+
+
+def bottom_up_matrix_multiplication(p):
+    n = len(p) - 1
+    m = numpy.zeros((n, n))
+    s = numpy.zeros((n - 1, n))
+
+    for l in xrange(2, n + 1):
+        for i in xrange(n - l + 1):
+            j = i + l - 1
+            m[i][j] = float("inf")
+
+            for k in xrange(i, j):
+                q = m[i][k] + m[k + 1][j] + p[i] * p[k + 1] * p[j + 1]
+                if q < m[i][j]:
+                    m[i][j] = q
+                    s[i][j] = int(k)
+
+    return m, s
+
+
+def print_optimal_matrix(s, i, j):
+    if i == j:
+        print 'A{i}'.format(i=i + 1),
+
+    else:
+        k = int(s[i][j])
+        print '(',
+        print_optimal_matrix(s, i, k)
+        print_optimal_matrix(s, k + 1, j)
+        print ')',
+
+
+def memoized_matrix_chain_one_based(p):
+    n = len(p) - 1
+    m = numpy.ones((n, n))
+    m *= float('inf')
+    return lookup_chain_one_based(m, p, 1, n)
+
+
+def lookup_chain_one_based(m, p, i, j):
+    if m[i - 1][j - 1] < float('inf'):
+        return m[i - 1][j - 1]
+
+    if i == j:
+        m[i - 1][j - 1] = 0
+
+    else:
+        for k in xrange(i, j):
+            q = lookup_chain_one_based(m, p, i, k) + \
+                lookup_chain_one_based(m, p, k + 1, j) + \
+                p[i - 1] * p[k] * p[j]
+
+            if q < m[i - 1][j - 1]:
+                m[i - 1][j - 1] = q
+
+    return m[i - 1][j - 1]
+
+
+def memoized_matrix_chain(p):
+    n = len(p) - 1
+    m = numpy.full((n, n), float('inf'))
+    return lookup_chain(m, p, 0, n - 1)
+
+
+def lookup_chain(m, p, i, j):
+    if m[i][j] < float('inf'):
+        return m[i][j]
+
+    if i == j:
+        m[i][j] = 0
+
+    else:
+        for k in xrange(i, j):
+            q = lookup_chain(m, p, i, k) + \
+                lookup_chain(m, p, k + 1, j) + \
+                p[i] * p[k + 1] * p[j + 1]
+
+            if q < m[i][j]:
+                m[i][j] = q
+
+    return m[i][j]
+
+
+def matrix_multiplication_testing():
+    p = [30, 35, 15, 5, 10, 20, 25]
+    ob_m, ob_s = bottom_up_matrix_multiplication_one_based(p)
+    m, s = bottom_up_matrix_multiplication(p)
+
+    # print_optimal_matrix(s, 0, 5)
+    print memoized_matrix_chain(p)
+
+
+def currency_trading_djikstra(rates, start_node, finish_node):
+    frontier = []
+    heapq.heappush(frontier, (-1 * rates[start_node][start_node], start_node, [start_node]))
+    results = {}
+
+    optimal_price = 0
+    optimal_path = None
+
+    while frontier:
+        current_price, current_node, current_path = heapq.heappop(frontier)
+        current_price *= -1
+
+        for next_node in xrange(len(rates)):
+            if (current_node == next_node) or (next_node == start_node):
+                continue
+
+            next_price = current_price * rates[current_node][next_node]
+            next_key = (current_path[0], next_node)
+
+            if (next_key not in results) or results[next_key][1] + 0.01 < next_price:
+                next_path = current_path + [next_node]
+                results[next_key] = (next_path, next_price)
+
+                if next_node == finish_node:
+                    if next_price > optimal_price + 0.01:
+                        optimal_price = next_price
+                        optimal_path = next_path
+                        print optimal_price, optimal_path
+
+                else:
+                    heapq.heappush(frontier, (-1 * next_price, next_node, next_path,))
+
+    return optimal_price, optimal_path
+
+
+def test_currency_trading():
+    rates = numpy.array([numpy.fromstring('1 0.741 0.657 1.061 1.005', sep=' '),
+                         numpy.fromstring('1.349 1 0.888 1.433 1.366', sep=' '),
+                         numpy.fromstring('1.521 1.126 1 1.614 1.538', sep=' '),
+                         numpy.fromstring('0.942 0.698 0.619 1 0.953', sep=' '),
+                         numpy.fromstring('0.995 0.732 0.650 1.049 1', sep=' ')])
+
+    print currency_trading_djikstra(rates, 1, 4)
+
+
+def memoized_game_strategy(coins, start=0, end=None, cache=None, start_player=1):
+    if cache is None:
+        cache = {}
+
+    if end is None:
+        end = len(coins) - 1
+
+    key = (start, end)
+    if key in cache:
+        return cache[key]
+
+    if start == end:
+        value = coins[start] * start_player
+
+    elif start + 1 == end:
+        value = numpy.abs(coins[start] - coins[end]) * start_player
+
+    else:
+        options = (start_player * coins[start] + memoized_game_strategy(coins, start + 1, end, cache, -1 * start_player),
+                   start_player * coins[end] + memoized_game_strategy(coins, start, end - 1, cache, -1 * start_player))
+
+        if 1 == start_player:
+            value = max(options)
+
+        else:
+            value = min(options)
+
+    cache[key] = value
+    return value
+
+
+def bottom_up_game_strategy(coins):
+    cache = {}
+    n = len(coins)
+
+    for game_length in xrange(1, n + 1):
+        start_player = (game_length % 2 == n % 2) and 1 or -1
+
+        for start in xrange(n - game_length + 1):
+            end = start + game_length - 1
+
+            key = (start, end)
+            if 1 == game_length:
+                value = coins[start] * start_player
+
+            elif 2 == game_length:
+                value = abs(coins[start] - coins[end]) * start_player
+
+            else:
+                options = (start_player * coins[start] + cache[(start + 1, end)],
+                           start_player * coins[end] + cache[(start, end - 1)])
+
+                if 1 == start_player:
+                    value = max(options)
+
+                else:
+                    value = min(options)
+
+            cache[key] = value
+
+    print cache
+
+    return cache[(0, n - 1)]
+
+
+def test_game_strategy():
+    coins = [2, 10, 1, 5, 3]
+    print memoized_game_strategy(coins)
+    print bottom_up_game_strategy(coins)
+
+if __name__ == '__main__':
+    # matrix_multiplication_testing()
+    # test_currency_trading()
+    test_game_strategy()
