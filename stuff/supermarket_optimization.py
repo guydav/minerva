31b10bc1c6af9ae7a4d38b98fe3ef5efcf485bc3
Guy Davidson <guydav@gmail.com>
2017-03-22 00:36:01 -0300

    Another WIP version

--- stuff/supermarket_optimization.py
+++ stuff/supermarket_optimization.py
@@ -1,9 +1,10 @@
 import argparse
-import heapq
+import collections
 import os
 import time
 import itertools
 import tqdm
+import bisect
 
 
 DATE_AND_TIME_FORMAT = '%Y_%m_%d_%H_%M_%S'
@@ -11,62 +12,11 @@ DEFAULT_MINIMUM_SIZE = 3
 DEFAULT_OUTPUT_FILE_TEMPLATE = 'supermarket_optimizer_output_{time}.dat'
 
 
-class MinHeap(object):
-    """
-    A lightweight OOP wrapper of Python's built-in heapq
-    """
-    def __init__(self, data=None):
-        if data is not None:
-            self.heap = heapq.heapify(data)
-
-        else:
-            self.heap = []
-
-    def push(self, item):
-        heapq.heappush(self.heap, item)
-
-    def pop(self):
-        return heapq.heappop(self.heap)
-
-    def push_pop(self, item):
-        return heapq.heappushpop(self.heap, item)
-
-    def n_smallest(self, n):
-        return heapq.nsmallest(n, self.heap)
-
-    def n_largest(self, n):
-        return heapq.nsmallest(n, self.heap)
-
-    def peek(self):
-        return self.heap[0]
-
-    def heapify(self):
-        heapq.heapify(self.heap)
-
-    def __len__(self):
-        return len(self.heap)
-
-    def __str__(self):
-        return str(self.heap)
-
-    def __getitem__(self, item):
-        return self.heap.__getitem__(item)
-
-
 class SupermarketOptimizer(object):
-    def __init__(self):
-        self.occurrence_table = {}
-        self.heap = MinHeap()
-
-    def _heap_push(self, line_tuple):
-        self.heap.push((-1 * len(line_tuple), line_tuple))
-
-    def _heap_pop(self):
-        return self.heap.pop()[1]
-
-    def _heap_pop_generator(self):
-        while len(self.heap):
-            yield self._heap_pop()
+    def __init__(self, min_group_size):
+        self.min_group_size = min_group_size
+        self.occurrence_table = collections.defaultdict(int)
+        self.groups_by_size = collections.defaultdict(list)
 
     def process_input(self, input_file, assume_unsorted=False):
         """
@@ -79,18 +29,18 @@ class SupermarketOptimizer(object):
         """
         for line in input_file:
             group = line.strip().split(' ')
+            group_size = len(group)
+
+            if len(group) < self.min_group_size:
+                continue
 
             if assume_unsorted:
                 group = sorted(group)
 
             group = tuple(group)
 
-            if group not in self.occurrence_table:
-                self.occurrence_table[group] = 1
-                self._heap_push(group)
-
-            else:
-                self.occurrence_table[group] += 1
+            self.occurrence_table[group] += 1
+            self.groups_by_size[group_size].append(group)
 
     def _output(self, group, group_count, output_file):
         """
@@ -104,30 +54,30 @@ class SupermarketOptimizer(object):
         output.extend(group)
         output_file.write(', '.join(output) + '\r\n')
 
-    def _break_down_group(self, group, group_count, min_group_size):
-        """
-        TODO: document
-        :param group:
-        :param group_count:
-        :param min_group_size:
-        :return:
-        """
-        for subgroup_size in range(len(group) - 1, min_group_size - 1, -1):
-            for subgroup in itertools.combinations(group, subgroup_size):
-                if subgroup in self.occurrence_table:
+    def _break_down_group(self, group, group_count, sorted_group_lengths):
+        start_index = bisect.bisect_left(sorted_group_lengths, len(group)) - 1
+        group_set = set(group)
+
+        for subgroup_length in sorted_group_lengths[start_index::-1]:
+            for subgroup in self.groups_by_size[subgroup_length]:
+                subgroup_set = set(subgroup)
+
+                if subgroup_set.issubset(group_set):
                     self.occurrence_table[subgroup] += group_count
 
-                    remainder_group = tuple(set(group).difference(subgroup))
-                    if len(remainder_group) < min_group_size:
+                    remainder_group = tuple(group_set.difference(subgroup_set))
+
+                    if len(remainder_group) < self.min_group_size:
                         return
 
                     if remainder_group in self.occurrence_table:
                         self.occurrence_table[remainder_group] += group_count
                     else:
                         self._break_down_group(remainder_group, group_count,
-                                               min_group_size)
+                                               sorted_group_lengths)
 
-    def find_supported_groups(self, support_level, min_group_size, output_file):
+
+    def find_supported_groups(self, support_level, output_file):
         """
         TODO: document
         :param support_level:
@@ -135,20 +85,20 @@ class SupermarketOptimizer(object):
         :param output_file:
         :return:
         """
-        for group in tqdm.tqdm(self._heap_pop_generator(),
-                               total=len(self.heap)):
-            # Since we are popping from a max-heap, once we reach a group
-            # below the minimum size, we know we are done
-            if len(group) < min_group_size:
-                output_file.flush()
-                return
+        sorted_group_lengths = list(sorted(self.groups_by_size.keys()))
 
-            group_count = self.occurrence_table[group]
-            if group_count >= support_level:
-                self._output(group, group_count, output_file)
+        for group_length in tqdm.tqdm(sorted_group_lengths[::-1],
+                                      total=len(self.occurrence_table)):
 
-            else:
-                self._break_down_group(group, group_count, min_group_size)
+            for group in self.groups_by_size[group_length]:
+                group_count = self.occurrence_table[group]
+
+                if group_count >= support_level:
+                    self._output(group, group_count, output_file)
+
+                else:
+                    self._break_down_group(group, group_count,
+                                           sorted_group_lengths)
 
         output_file.flush()
 
@@ -207,11 +157,10 @@ def main():
                             .format(size=DEFAULT_MINIMUM_SIZE))
 
     args = parser.parse_args()
-    optimizer = SupermarketOptimizer()
+    optimizer = SupermarketOptimizer(args.group_size)
     optimizer.process_input(args.input_file, args.assume_unsorted)
     with open(args.output_file, 'w') as output_file:
-        optimizer.find_supported_groups(args.support_level, args.group_size,
-                                        output_file)
+        optimizer.find_supported_groups(args.support_level, output_file)
 
 if __name__ == '__main__':
     main()
