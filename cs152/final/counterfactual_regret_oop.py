860f8d4038173fd787173dc3c78857e5e3733c58
Guy Davidson <guydav@gmail.com>
2017-12-09 15:25:53 +0900

    OOP'd the CFR code, and now it works the same, but is cleaner to play wih

---
+++ cs152/final/counterfactual_regret_oop.py
@@ -0,0 +1,328 @@
+import numpy as np
+from collections import defaultdict, Counter
+from bitarray import bitarray
+
+
+class CounterfactualRegretNode:
+    def __init__(self, actions, info_set, info_set_formatter=lambda x: str(x)):
+        self.actions = actions
+        self.info_set = info_set
+        self.info_set_formatter = info_set_formatter
+
+        self.num_actions = len(actions)
+        self.regret_sums = np.zeros((self.num_actions,), dtype=np.float64)
+        self.strategy_sums = np.zeros((self.num_actions,), dtype=np.float64)
+
+    def get_strategy(self, realization_weight):
+        strategy = np.where(self.regret_sums > 0, self.regret_sums, 0)
+        normalization = np.sum(strategy)
+
+        if normalization > 0:
+            strategy /= normalization
+
+        else:
+            strategy = np.ones_like(strategy) / self.num_actions
+
+        self.strategy_sums += strategy * realization_weight
+        return strategy
+
+    def get_average_strategy(self):
+        normalization = np.sum(self.strategy_sums)
+
+        if normalization > 0:
+            return self.strategy_sums / normalization
+
+        return np.ones_like(self.strategy_sums) / self.num_actions
+
+    def __repr__(self):
+        return '{info}: {avg}'.format(info=self.info_set_formatter(self.info_set),
+                                      avg=np.array_str(self.get_average_strategy(), precision=3))
+
+
+class CounterfactualRegretTrainer:
+    """
+    This implementation currently assumes the chance-sampling Monte Carlo formulation of CFR
+    Where the chance states are sampled prior to each training iteration.
+
+    TODO: consider implementing a chance-node supporting implementation
+    """
+    def __init__(self):
+        self.nodes = {}
+        self.total_utility = 0
+
+    def __call__(self, num_iterations, num_prints=None, should_print_result=False):
+        """
+        The external callable for this trainer
+        :param num_iterations: How many iterations should be trained
+        :param num_prints: How often should a status be printed - by default 100 times over
+            the entire training process, or once every 100 iterations, the smaller of the two
+        :param should_print_result: Should the final average strategy be printed
+        :return: None
+        """
+        if num_prints is None:
+            num_prints = min(num_iterations // 100, 100)
+
+        for t in range(num_iterations):
+            chance_state = self._chance_sampler(t)
+            initial_history = self._initial_history_generator()
+            self.total_utility += self._train(chance_state, initial_history)
+
+            if 0 == t % int(num_iterations / num_prints):
+                print('After {t} iterations, average utility = {util:.3f}'.format(
+                    t=t, util=self.total_utility / (t + 1)))
+
+        if should_print_result:
+            for info_set in sorted(self.nodes):
+                print(self.nodes[info_set])
+
+    def _train(self, chance_state, history, probabilities=None, player=0):
+        """
+        Run a single iteration of counterfactual regret minimization learning
+        :param chance_state: The chance-state for the current iteration, under
+            the chance-sampling Monte Carlo formulation of CFR
+        :param history: The current history of the game
+        :param probabilities: The probability of arriving at the current state
+            for each player
+        :param player: The ordinal of the current player
+        :return: The utility for the current player of the current state
+        """
+        opponent = 1 - player
+
+        if probabilities is None:
+            probabilities = np.ones((2,))
+
+        utility = self._check_terminal_state(chance_state, history)
+        if utility is not None:
+            return utility
+
+        info_set = self._information_set_generator(chance_state, history, player)
+        if info_set not in self.nodes:
+            self.nodes[info_set] = CounterfactualRegretNode(
+                self._node_action_generator(chance_state, history, player),
+                info_set, self._information_set_formatter_generator())
+
+        node = self.nodes[info_set]
+
+        # recursively CFR down the tree, returning utilities
+        strategy = node.get_strategy(probabilities[player])
+        utility = []
+        node_utility = 0
+        for action, action_prob in zip(node.actions, strategy):
+            new_prob = np.copy(probabilities)
+            new_prob[player] *= action_prob
+
+            action_utility = -1 * self._train(chance_state, self._action_history_updater(history, action),
+                                              new_prob, 1 - player)
+            utility.append(action_utility)
+            node_utility += action_utility * action_prob
+
+        utility = np.asarray(utility)
+
+        # compute counterfactual regret, weighted by probability of opponent arriving here
+        node.regret_sums += (utility - node_utility) * probabilities[opponent]
+
+        # return the node utility to propagate up the tree
+        return node_utility
+
+    def _chance_sampler(self, iteration=None):
+        """
+        Implement the chance sampler for this game
+        :return: the chance state this game assumes
+        """
+        raise NotImplementedError()
+
+    def _initial_history_generator(self):
+        """
+        Generate an initial history for the game, based on how you choose o represent it
+        :return: the initial history in a format the rest of the game assumes
+        """
+        raise NotImplementedError()
+
+    def _check_terminal_state(self, chance_state, history):
+        """
+        Check the current state of the game, between the chance state and the history,
+        do determine if the game is over, and if it is, return the utility to the current player
+        :param chance_state: The chance state of the game
+        :param history: The history of the game
+        :return: None if the game is not over, or the utility if the game is
+        """
+        raise NotImplementedError()
+
+    def _information_set_generator(self, chance_state, history, player):
+        """
+        Map the chance state, history, and current player to the key idnetifying the information set
+        :param chance_state: The chance state of the game
+        :param history: The history of the game
+        :param player: The ordinal of the current player
+        :return: The key identifying the information set (should be hashable)
+        """
+        raise NotImplementedError()
+
+    def _information_set_formatter_generator(self):
+        """
+        If you want to format the information sets more nicely for printing, override this
+        :return: A function that maps from information set to string
+        """
+        return lambda x: str(x)
+
+    def _node_action_generator(self, chance_state, history, player):
+        """
+        Generate the identifiers of the actions that could be taken from the node
+        representing the current game state
+        :param chance_state: The chance state of the game
+        :param history: The history of the game
+        :param player: The ordinal of the current player
+        :return: An iterable of actions that can be taken from the current game state
+        """
+        raise NotImplementedError()
+
+    def _action_history_updater(self, history, action):
+        """
+        Update the history with the latest action taken, based on how you choose
+        to represent the history and the actions
+        :param history: The history of the game
+        :param action: The identifier for the current action taken
+        :return: A new history including the latest action
+        """
+        raise NotImplementedError()
+
+
+# Kuhn poker related definitions
+PASS = 0
+BET = 1
+KUHN_POKER_ACTIONS = (PASS, BET)
+PASS_HISTORY = 'p'
+BET_HISTORY = 'b'
+KUHN_POKER_ACTION_TO_HISTORY = {
+    PASS: PASS_HISTORY,
+    BET: BET_HISTORY,
+}
+
+
+class KuhnPokerCFRTrainer(CounterfactualRegretTrainer):
+    def __init__(self):
+        super().__init__()
+        self.cards = np.asarray(range(3))
+
+    def _chance_sampler(self, iteration=None):
+        np.random.shuffle(self.cards)
+        return self.cards
+
+    def _check_terminal_state(self, chance_state, history):
+        plays = len(history)
+        player = plays % 2
+        opponent = 1 - player
+
+        if plays <= 1:
+            return None
+
+        player_wins = 1 if chance_state[player] > chance_state[opponent] else -1
+
+        # double bet or double pass
+        if history[-1] == history[-2]:
+            double_bet = 2 if history[-1] == BET_HISTORY else 1
+            return player_wins * double_bet
+
+        # other player just passed
+        if history[-1] == PASS_HISTORY:
+            return 1
+
+        return None
+
+    def _information_set_generator(self, chance_state, history, player):
+        return str(chance_state[player]) + history
+
+    def _node_action_generator(self, chance_state, history, player):
+        return KUHN_POKER_ACTIONS
+
+    def _initial_history_generator(self):
+        return ''
+
+    def _action_history_updater(self, history, action):
+        return history + KUHN_POKER_ACTION_TO_HISTORY[action]
+
+
+# Dudo-related definitions
+
+# borrowed from http://christophe-simonis-at-tiny.blogspot.kr/2008/08/python-reverse-enumerate.html
+def reverse_enumerate(l):
+    return zip(range(len(l) - 1, -1, -1), reversed(l))
+
+
+DUDO_DEFAULT_DIE_SIZE = 6
+DUDO_NUM_PLAYERS = 2
+
+
+class DudoTwoPlayerSingleDieCFRTrainer(CounterfactualRegretTrainer):
+    def __init__(self, die_size=DUDO_DEFAULT_DIE_SIZE):
+        super().__init__()
+        self.die_size = die_size
+
+        self.die = np.asarray(range(1, self.die_size + 1))
+        self.claim_actions = DUDO_NUM_PLAYERS * self.die_size
+        self.num_actions = self.claim_actions + 1
+        self.action_index_to_action = {i: (i // self.die_size + 1,
+                                           (((i % self.die_size) + 1) % self.die_size) + 1)
+                                       for i in range(self.claim_actions)}
+
+    def _initial_history_generator(self):
+        return bitarray([0] * self.num_actions)
+
+    def _chance_sampler(self, iteration=None):
+        return np.random.choice(self.die, 2, True)
+
+    def _check_terminal_state(self, chance_state, history):
+        if not history[-1]:
+            return None
+
+        # omit the last item of history to ignore the 'dudo' action
+        for index, value in reverse_enumerate(history[:-1]):
+            if value:
+                break
+
+        number, rank = self.action_index_to_action[index]
+        counter = Counter(chance_state)
+        relevant_die = counter[1]
+
+        # 1's are wild and always count, so if the rank isn't 1 we also count occurrences of the result
+        if 1 != rank:
+            relevant_die += counter[rank]
+
+        return 1 if relevant_die >= number else -1
+
+    def _information_set_formatter_generator(self):
+        def dudo_info_set_to_str(info_set):
+            dice_mod = 2 ** self.num_actions
+            d = info_set // dice_mod
+            h = bin(info_set - (dice_mod * d))[2:]
+            return '{d}, {h}'.format(d=d, h=h.rjust(self.num_actions, '0'))
+
+        return dudo_info_set_to_str
+
+    def _information_set_generator(self, chance_state, history, player):
+        return ((2 ** self.num_actions) * chance_state[player]) + int(history.to01(), 2)
+
+    def _node_action_generator(self, chance_state, history, player):
+        # if no bets have been made, you cannot claim 'dudo'
+        if not history.any():
+            return range(self.claim_actions)
+
+        # from the first unclaimed bet to 'dudo'
+        for index, value in reverse_enumerate(history[:-1]):
+            if value:
+                break
+
+        return range(index + 1, self.num_actions)
+
+    def _action_history_updater(self, history, action):
+        new_history = history.copy()
+        new_history[action] = True
+        return new_history
+
+
+if __name__ == '__main__':
+    # trainer = KuhnPokerCFRTrainer()
+    # trainer(10000, should_print_result=True)
+
+    dudo_trainer = DudoTwoPlayerSingleDieCFRTrainer()
+    dudo_trainer(10000, should_print_result=True)
